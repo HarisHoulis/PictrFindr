6d2c12e4d7f57f7f56f9d62a4c602632b416db3d
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-01-31 18:38:16 +0100

    Create first convention plugin for Compose app

---
+++ build-logic/convention/src/main/kotlin/compose-app.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+private val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+android {
+    namespace = "com.houlis.haris.pictrfindr"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.houlis.haris.pictrfindr"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        allWarningsAsErrors = true
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-opt-in=kotlin.RequiresOptIn",
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-opt-in=kotlinx.coroutines.FlowPreview",
+        )
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
+    }
+
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform()
+        }
+    }
+}
+
+
+dependencies {
+    val bom = libs.findLibrary("androidx-compose-bom").get()
+    implementation(platform(bom))
+    androidTestImplementation(platform(bom))
+}
