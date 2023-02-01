cd57aa8ba80882d5a8507a452ae939ad4acb3ba1
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-01 14:38:25 +0100

    Add android feature Convention plugin

---
+++ build-logic/convention/src/main/kotlin/android-feature.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("android-lib")
+    id("android-hilt")
+}
+
+dependencies {
+    add("implementation", project(":core:network"))
+    add("implementation", libs.getLib("androidx.hilt.navigation.compose"))
+    add("implementation", libs.getLib("androidx.lifecycle.runtimeCompose"))
+    add("implementation", libs.getLib("androidx.lifecycle.viewModelCompose"))
+    add("implementation", libs.getLib("coil.kt"))
+    add("implementation", libs.getLib("coil.kt.compose"))
+    add("implementation", libs.getLib("kotlinx.coroutines.android"))
+
+//    add("testImplementation", project(":core:test"))
+    add("testImplementation", kotlin("test"))
+
+//    add("androidTestImplementation", project(":core:test"))
+    add("androidTestImplementation", kotlin("test"))
+}
