b15d00c5396b99d92bd4a8f51e83910cff095ad7
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-01 10:58:25 +0100

    Add plugins via the VersionCatalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,9 @@ buildscript {
 }
 
   plugins {
-    id("com.android.application") version "8.0.0-beta01" apply false
-    id ("com.android.library") version "8.0.0-beta01" apply false
-    id ("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id ("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
-}
+      // False-positive error for `libs`: https://github.com/gradle/gradle/issues/22797
+      alias(libs.plugins.android.application) apply false
+      alias(libs.plugins.kotlin.jvm) apply false
+      alias(libs.plugins.kotlin.serialization) apply false
+      alias(libs.plugins.hilt) apply false
+  }
