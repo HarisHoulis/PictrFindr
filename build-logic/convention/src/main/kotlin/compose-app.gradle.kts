805cc0bd2dc1f6cdfa5e521bade67dbdefa5331e
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-01 12:23:50 +0100

    Use JAVA 17

--- build-logic/convention/src/main/kotlin/compose-app.gradle.kts
+++ build-logic/convention/src/main/kotlin/compose-app.gradle.kts
@@ -28,13 +28,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         allWarningsAsErrors = true
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
         freeCompilerArgs = freeCompilerArgs + listOf(
             "-opt-in=kotlin.RequiresOptIn",
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
