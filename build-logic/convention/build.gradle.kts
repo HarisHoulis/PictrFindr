bd8d7c49ee8399fb5ebfb85fa55bd8ea3b86f31e
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-02 14:28:33 +0100

    Remove unnecessary block
    This is already set by the project's embedded JDK

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
 
 group = "com.houlis.haris.pictrfindr.buildlogic"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
 
 dependencies {
     implementation(libs.android.gradlePlugin)
