ad1552fad2093b869f47a2db436b176d587b249c
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-03 18:06:09 +0100

    Bind app

---
+++ app/src/main/kotlin/com/houlis/haris/pictrfindr/ui/Navigation.kt
@@ -0,0 +1,16 @@
+package com.houlis.haris.pictrfindr.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.houlis.haris.list.ui.navigation.picturesGraph
+import com.houlis.haris.list.ui.navigation.picturesRoute
+
+@Composable
+fun MainNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = picturesRoute) {
+        picturesGraph()
+    }
+}
