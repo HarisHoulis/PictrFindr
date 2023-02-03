9cdf860a8946a35ceb96296057ff5053d5c9e5ea
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-03 15:38:41 +0100

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,17 +3,7 @@ plugins {
 }
 
 dependencies {
-
-    debugImplementation (libs.androidx.compose.ui.tooling)
-    debugImplementation (libs.androidx.compose.ui.tooling.preview)
-
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.lifecycle.runtimeCompose)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui)
-
-    //TODO check if we need that: implementation "androidx.compose.ui:ui.graphics"
+    implementation(project(":feature:list"))
 
     testImplementation(libs.junit5.bom)
     testImplementation(libs.junit.jupiter)
@@ -22,5 +12,4 @@ dependencies {
     androidTestImplementation (libs.androidx.test.ext)
     androidTestImplementation (libs.androidx.test.espresso.core)
     androidTestImplementation (libs.androidx.compose.ui.test)
-
 }
