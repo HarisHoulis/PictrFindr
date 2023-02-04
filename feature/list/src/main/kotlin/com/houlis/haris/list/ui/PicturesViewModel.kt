156b664ac49822750c1d10df878d0fcfc0ba92ec
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-04 17:08:25 +0100

    Remove unnecessary Job
    Operator FLow.collectLatest takes care of handling the cancellation of the previous running block, if the value of queryFlow changes.

--- feature/list/src/main/kotlin/com/houlis/haris/list/ui/PicturesViewModel.kt
+++ feature/list/src/main/kotlin/com/houlis/haris/list/ui/PicturesViewModel.kt
@@ -3,44 +3,52 @@ package com.houlis.haris.list.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.houlis.haris.list.api.PicturesRepositoryContract
+import com.houlis.haris.list.ui.PicturesUiState.Type
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Success
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 internal class PicturesViewModel @Inject constructor(
-    val repository: PicturesRepositoryContract,
+    private val repository: PicturesRepositoryContract,
 ) : ViewModel() {
 
-    private var searchJob: Job? = null
-
-    private val _state: MutableStateFlow<PicturesUiState> = MutableStateFlow(PicturesUiState.Loading)
+    private val _state: MutableStateFlow<PicturesUiState> = MutableStateFlow(PicturesUiState())
     val state = _state.asStateFlow()
 
-    fun searchFor(query: String) {
-        searchJob?.cancel()
-        searchJob = viewModelScope.launch {
-            val newState = when (val result = repository.searchFor(query)) {
-                is Failure -> PicturesUiState.Error
-                is Success ->
-                    if (result.value.isEmpty())
-                        PicturesUiState.Empty
-                    else
-                        PicturesUiState.Fetched(result.value)
-            }
-            _state.update { newState }
+    private val queryFLow = state
+        .map { it.input }
+        .distinctUntilChanged()
+
+    init {
+        viewModelScope.launch {
+            queryFLow
+                .filterNot(String::isEmpty)
+                .distinctUntilChanged()
+                .collectLatest { query ->
+                    val newState = when (val result = repository.searchFor(query)) {
+                        is Failure -> Type.Error
+                        is Success ->
+                            if (result.value.isEmpty())
+                                Type.Empty
+                            else
+                                Type.Fetched(result.value)
+                    }
+                    _state.update { it.copy(type = newState) }
+                }
         }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        searchJob?.cancel()
-        searchJob = null
+    fun searchFor(query: String) {
+        _state.update { it.copy(input = query) }
     }
 }
