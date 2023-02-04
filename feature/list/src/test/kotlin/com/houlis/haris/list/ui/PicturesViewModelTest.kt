835ceaec8ac01522658c495da4c39a38c2c8efbb
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-04 17:09:47 +0100

    Debounce user input
    Only accept user queries once they have stopped typing.

--- feature/list/src/test/kotlin/com/houlis/haris/list/ui/PicturesViewModelTest.kt
+++ feature/list/src/test/kotlin/com/houlis/haris/list/ui/PicturesViewModelTest.kt
@@ -6,6 +6,9 @@ import com.houlis.haris.list.domain.IMAGE_BASE_URL
 import com.houlis.haris.list.domain.dummyPicture3
 import com.houlis.haris.list.domain.dummyPicture4
 import com.houlis.haris.list.domain.dummyPictures
+import com.houlis.haris.list.ui.PicturesUiState.Type
+import com.houlis.haris.list.ui.PicturesUiState.Type.Fetched
+import com.houlis.haris.list.ui.PicturesUiState.Type.Loading
 import com.houlis.haris.network.data.PicturesApi
 import com.houlis.haris.test.data.TestPicturesApi
 import com.houlis.haris.test.data.TestPicturesApi.Query.Query1
@@ -18,6 +21,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.time.Duration
 
 @ExtendWith(MainTestDispatcherExtension::class)
 internal class PicturesViewModelTest {
@@ -25,31 +29,33 @@ internal class PicturesViewModelTest {
     companion object {
         @JvmStatic
         fun `api to expected UI state`() = listOf(
-            Arguments.of(testPicturesApi(), PicturesUiState.Fetched(dummyPictures())),
-            Arguments.of(testPicturesApi().apply { setEmptyResponse() }, PicturesUiState.Empty),
-            Arguments.of(testPicturesApi().apply { throwException() }, PicturesUiState.Error),
+            Arguments.of(testPicturesApi(), Fetched(dummyPictures())),
+            Arguments.of(testPicturesApi().apply { setEmptyResponse() }, Type.Empty),
+            Arguments.of(testPicturesApi().apply { throwException() }, Type.Error),
         )
 
         private fun testPicturesApi() = TestPicturesApi()
     }
 
-    private fun testedClass(api: PicturesApi = testPicturesApi()) =
+    private fun testedClass(api: PicturesApi = testPicturesApi(), debounce: Duration = Duration.ofMillis(0)) =
         PicturesViewModel(
-            PicturesRepository(api, IMAGE_BASE_URL)
+            PicturesRepository(api, IMAGE_BASE_URL),
+            debounce
         )
 
     @Test
     fun `initial state is loading`() = runTest {
         testedClass().state.test {
-            awaitItem() shouldBe PicturesUiState.Loading
+            awaitItem() shouldBe PicturesUiState("", Loading)
             expectNoEvents()
         }
     }
 
     @ParameterizedTest
     @MethodSource("api to expected UI state")
-    fun `reduces state`(api: PicturesApi, expectedUiState: PicturesUiState) = runTest {
+    fun `reduces state`(api: PicturesApi, expectedUiStateType: Type) = runTest {
         // ARRANGE
+        val initialState = PicturesUiState()
         val testedClass = testedClass(api)
 
         // ACT
@@ -57,8 +63,9 @@ internal class PicturesViewModelTest {
             testedClass.searchFor(Query1.text)
 
             // ASSERT
-            awaitItem() shouldBe PicturesUiState.Loading
-            awaitItem() shouldBe expectedUiState
+            awaitItem() shouldBe initialState
+            awaitItem() shouldBe initialState.copy(input = Query1.text)
+            awaitItem() shouldBe initialState.copy(Query1.text, expectedUiStateType)
             expectNoEvents()
         }
     }
@@ -66,7 +73,8 @@ internal class PicturesViewModelTest {
     @Test
     fun `reduces state based on most recent query`() = runTest {
         // ARRANGE
-        val testedClass = testedClass()
+        val initialState = PicturesUiState("", Loading)
+        val testedClass = testedClass(debounce = Duration.ofMillis(10))
 
         // ACT
         testedClass.state.test {
@@ -74,8 +82,10 @@ internal class PicturesViewModelTest {
             testedClass.searchFor(Query2.text)
 
             // ASSERT
-            awaitItem() shouldBe PicturesUiState.Loading
-            awaitItem() shouldBe PicturesUiState.Fetched(listOf(dummyPicture3(), dummyPicture4()))
+            awaitItem() shouldBe initialState
+            awaitItem() shouldBe initialState.copy(input = Query1.text)
+            awaitItem() shouldBe initialState.copy(input = Query2.text)
+            awaitItem() shouldBe initialState.copy(Query2.text, type = Fetched(listOf(dummyPicture3(), dummyPicture4())))
             expectNoEvents()
         }
     }
