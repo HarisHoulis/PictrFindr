b606d048bc7d486d807cf62d29ad381784acdb58
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-05 18:08:33 +0100

    Add ability to store/retrieve clicked Picture

--- core/domain/src/main/kotlin/com/houlis/haris/core/domain/PicturesRepositoryContract.kt
+++ core/domain/src/main/kotlin/com/houlis/haris/core/domain/PicturesRepositoryContract.kt
@@ -5,4 +5,8 @@ import dev.forkhandles.result4k.Result4k
 interface PicturesRepositoryContract {
 
     suspend fun searchFor(query: String): Result4k<List<Picture>, Nothing?>
+
+    suspend fun save(picture: Picture)
+
+    suspend fun retrieve(picId: String): Picture
 }
