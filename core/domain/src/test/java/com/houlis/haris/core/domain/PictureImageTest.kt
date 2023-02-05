e68d31b79e224ec4220a9186804c21fa26a8507a
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-05 18:10:01 +0100

    Move under proper package

--- core/domain/src/test/java/com/houlis/haris/core/domain/PictureImageTest.kt
+++ core/domain/src/test/java/com/houlis/haris/core/domain/PictureImageTest.kt
@@ -1,6 +1,5 @@
-package com.houlis.haris.feature.list.domain
+package com.houlis.haris.core.domain
 
-import com.houlis.haris.feature.list.domain.Picture
 import com.houlis.haris.test.data.provider.provider.dummyPictureRaw1
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
