786a0a50ea4f8a7e8a6f2fec7376c5ad8f7d2d28
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-05 10:17:47 +0100

    Implement PicturesScreen

---
+++ feature/list/src/androidTest/java/com/houlis/haris/list/PicturesScreenTest.kt
@@ -0,0 +1,50 @@
+package com.houlis.haris.list
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.houlis.haris.list.ui.PicturesScreen
+import com.houlis.haris.list.ui.PicturesUiState
+import com.houlis.haris.list.ui.PicturesUiState.Type.Empty
+import com.houlis.haris.list.ui.PicturesUiState.Type.Error
+import org.junit.Rule
+import org.junit.Test
+
+class PicturesScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private fun launch(state: PicturesUiState = PicturesUiState()) {
+        composeTestRule.setContent {
+            PicturesScreen(state = state, onValueChange = {})
+        }
+    }
+
+    @Test
+    fun prompts_user_to_search() {
+        launch()
+
+        composeTestRule
+            .onNodeWithText("Go search for sth!")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun shows_no_results_were_found() {
+        launch(PicturesUiState("", Empty))
+
+        composeTestRule
+            .onNodeWithText("No items were found. Please try again.")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun shows_error() {
+        launch(PicturesUiState("", Error))
+
+        composeTestRule
+            .onNodeWithText("Sth went wrong, please try again later.")
+            .assertIsDisplayed()
+    }
+}
