d48bd090292fd914bc7f147c52b5ae3541b993c9
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-02-06 09:20:34 +0100

    Explain reason of missing suppression

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
 
   plugins {
       // False-positive error for `libs`: https://github.com/gradle/gradle/issues/22797
+      // Suppression doesn't work here :(
       alias(libs.plugins.android.application) apply false
       alias(libs.plugins.kotlin.jvm) apply false
       alias(libs.plugins.kotlin.serialization) apply false
