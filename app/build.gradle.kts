80dd212df959b7dc2c5d533f124bfa4c36ad2518
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-05-18 12:46:38 +0200

    chore: Add spotlessCheck as pre-push hook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,3 +6,13 @@ dependencies {
     implementation(project(":feature:list"))
     implementation(project(":feature:details"))
 }
+
+afterEvaluate {
+    tasks
+        .filter { task ->
+            task.name.equals("clean", ignoreCase = true) ||
+                    task.name.contains("assemble", ignoreCase = true)
+        }.forEach { task ->
+            task.dependsOn(":installGitHooks")
+        }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,17 @@ buildscript {
 
 }
 
-  plugins {
-      // False-positive error for `libs`: https://github.com/gradle/gradle/issues/22797
-      // Suppression doesn't work here :(
-      alias(libs.plugins.android.application) apply false
-      alias(libs.plugins.kotlin.jvm) apply false
-      alias(libs.plugins.kotlin.serialization) apply false
-      alias(libs.plugins.hilt) apply false
-  }
+plugins {
+    // False-positive error for `libs`: https://github.com/gradle/gradle/issues/22797
+    // Suppression doesn't work here :(
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+}
+
+tasks.register<Copy>("installGitHooks") {
+    from(layout.projectDirectory.dir("scripts/hooks"))
+    into(File(rootDir, "/.git/hooks"))
+    fileMode = 775
+}
